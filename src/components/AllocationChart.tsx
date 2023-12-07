6856cef1905eefe767c0beee5e3f17d5a6f0cfe2
Cláudio Barreira <claudio.barreira89@gmail.com>
2023-12-07 12:48:14 +0100

    feat: sc integrations

--- src/components/AllocationChart.tsx
+++ src/components/AllocationChart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   BarChart,
   Bar,
@@ -9,19 +9,73 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import { colors } from "../../tailwind.config";
+import { useContractRead } from "wagmi";
+import vaultAbi from "../constants/vaultAbi.json";
+import Spinner from "./Spinner";
+import { fetchBalance } from "wagmi/actions";
 
 const AllocationChart = () => {
-  const data = [
-    { name: "BTC", allocation: 30 },
-    { name: "ETH", allocation: 20 },
-    { name: "DOGE", allocation: 10 },
-    { name: "SOL", allocation: 15 },
-    { name: "BNB", allocation: 25 },
-  ];
+  const [allocations, setAllocations] = useState<
+    { name: string; allocation: number }[]
+  >([]);
+
+  const supportedTokens = useContractRead({
+    address: process.env.NEXT_PUBLIC_VAULT_CONTRACT_ADDRESS! as `0x${string}`,
+    abi: vaultAbi,
+    functionName: "supportedTokens",
+  });
+
+  const totalValue = useContractRead({
+    address: process.env.NEXT_PUBLIC_VAULT_CONTRACT_ADDRESS! as `0x${string}`,
+    abi: vaultAbi,
+    functionName: "calculateTotalValue",
+  });
+
+  const fetchAllocations = useCallback(async () => {
+    // if (!supportedTokens?.data || totalValue.data) return;
+
+    let tempAllocations = [];
+
+    // for (let tokenAddress of supportedTokens.data) {
+    //   const balance = await fetchBalance({
+    //     address: process.env
+    //       .NEXT_PUBLIC_VAULT_CONTRACT_ADDRESS! as `0x${string}`,
+    //     token: tokenAddress,
+    //   });
+
+    //   const proportion = Number(balance.value) / Number(totalValue.data);
+
+    //   tempAllocations.push({ tokenAddress, proportion });
+    // }
+
+    const data = [
+      { name: "BTC", allocation: 30 },
+      { name: "ETH", allocation: 20 },
+      { name: "DOGE", allocation: 10 },
+      { name: "SOL", allocation: 15 },
+      { name: "BNB", allocation: 25 },
+    ];
+
+    setAllocations(data);
+  }, []);
+
+  useEffect(() => {
+    fetchAllocations();
+  }, []);
+
+  const isLoading = totalValue.isLoading || supportedTokens.isLoading;
+
+  if (isLoading) {
+    return (
+      <div className="h-48 flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
 
   return (
     <ResponsiveContainer width="100%" height={300}>
-      <BarChart data={data}>
+      <BarChart data={allocations}>
         <XAxis dataKey="name" />
         <YAxis domain={[0, 100]} />
         <Tooltip
--- src/components/SwapForm.tsx
+++ src/components/SwapForm.tsx
@@ -1,20 +1,125 @@
 import classNames from "classnames";
 import Typography from "./Typography";
-import { useAccount, useConnect } from "wagmi";
+import {
+  useAccount,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from "wagmi";
 import Button from "./Button";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import vaultAbi from "../constants/vaultAbi.json";
+import { ChangeEvent, ReactNode, useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
+import Spinner from "./Spinner";
+
+type SwapTypes = "deposit" | "withdraw";
 
 const SwapForm = () => {
+  const [swapType, setSwapType] = useState<SwapTypes>("deposit");
+  const [sellAmount, setSellAmount] = useState<string>("");
+  const [buyAmount, setBuyAmount] = useState<string>("");
   const { address } = useAccount();
-  const { connect } = useConnect();
+
+  const [debouncedSellAmount] = useDebounce(sellAmount, 1000);
+
+  const { data, isLoading: isFetchingSharesValue } = useContractRead({
+    address: process.env.NEXT_PUBLIC_VAULT_CONTRACT_ADDRESS! as `0x${string}`,
+    abi: vaultAbi,
+    functionName: "calculateShares",
+    enabled: !!sellAmount && !!debouncedSellAmount && swapType === "deposit",
+    args: [debouncedSellAmount],
+  });
+
+  const deposit = useContractWrite({
+    address: process.env.NEXT_PUBLIC_VAULT_CONTRACT_ADDRESS! as `0x${string}`,
+    abi: vaultAbi,
+    functionName: "deposit",
+  });
+
+  const withdraw = useContractWrite({
+    address: process.env.NEXT_PUBLIC_VAULT_CONTRACT_ADDRESS! as `0x${string}`,
+    abi: vaultAbi,
+    functionName: "withdraw",
+  });
+
+  const handleSellInputChange = (v: string) => {
+    setSellAmount(v);
+  };
+
+  const handleBuyInputChange = (v: string) => {
+    setBuyAmount(v);
+  };
+
+  const handleDeposit = () => {
+    deposit.write({
+      args: [sellAmount],
+    });
+  };
+
+  const handleWithdraw = () => {};
+
+  // const deposit = useContractRead({
+  //   address: process.env.NEXT_PUBLIC_VAULT_CONTRACT_ADDRESS! as `0x${string}`,
+  //   abi: vaultAbi,
+  //   functionName: "deposit",
+  // });
+
+  // const withdraw = usePrepareContractWrite({
+  //   address: process.env.NEXT_PUBLIC_VAULT_CONTRACT_ADDRESS! as `0x${string}`,
+  //   abi: vaultAbi,
+  //   functionName: "withdraw",
+  // });
+
+  useEffect(() => {
+    setSellAmount("");
+    setBuyAmount("");
+  }, [swapType]);
+
+  const txLoading = deposit.isLoading || withdraw.isLoading;
 
   return (
     <div className="shadow-card rounded-card bg-white dark:bg-primary-950 w-full">
-      <TokenInput label="Sell" token="USDC" />
-      <TokenInput label="Buy" token="HDG" className="border-t" />
+      <div className="rounded-full border flex justify-between dark:text-white m-6 overflow-hidden">
+        <TabButton
+          isActive={swapType === "deposit"}
+          onClick={() => setSwapType("deposit")}
+        >
+          Deposit
+        </TabButton>
+        <TabButton
+          isActive={swapType === "withdraw"}
+          onClick={() => setSwapType("withdraw")}
+        >
+          Withdraw
+        </TabButton>
+      </div>
+
+      <TokenInput
+        label="Sell"
+        token={swapType === "deposit" ? "USDC" : "VTK"}
+        value={sellAmount}
+        onChange={(e) => handleSellInputChange(e.target.value)}
+      />
+
+      <TokenInput
+        label="Buy"
+        token={swapType === "withdraw" ? "USDC" : "VTK"}
+        className="border-t"
+        value={buyAmount}
+        isLoading={isFetchingSharesValue}
+        disabled={true}
+        onChange={(e) => handleBuyInputChange(e.target.value)}
+      />
+
       <div className="p-6 border-t">
         {address ? (
-          <Button>Confirm</Button>
+          <Button
+            onClick={swapType === "deposit" ? handleDeposit : handleWithdraw}
+            isLoading={txLoading}
+          >
+            Confirm
+          </Button>
         ) : (
           <ConnectButton.Custom>
             {({ openConnectModal }) => (
@@ -27,24 +132,61 @@ const SwapForm = () => {
   );
 };
 
+const TabButton = ({
+  isActive,
+  children,
+  onClick,
+}: {
+  isActive: boolean;
+  children: ReactNode;
+  onClick: () => void;
+}) => (
+  <div
+    onClick={onClick}
+    className={classNames(
+      "p-2 flex-1 text-center rounded-full cursor-pointer",
+      { ["bg-primary-600 text-white"]: isActive }
+    )}
+  >
+    {children}
+  </div>
+);
+
 const TokenInput = ({
   label,
   token,
+  value,
   className,
+  isLoading,
+  disabled,
+  onChange,
 }: {
   label?: string;
   token?: string;
+  value?: string;
   className?: string;
+  isLoading?: boolean;
+  disabled?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }) => {
   return (
     <div className={classNames("p-6 w-full dark:text-white", className)}>
       {label && <div className="mb-2">{label}</div>}
       <div className="flex gap-2 items-center ">
-        <input
-          type="number"
-          placeholder="0.0"
-          className="dark:text-white border-none appearance-none outline-none whitespace-nowrap overflow-ellipsis w-full text-3xl flex-1 bg-transparent"
-        />
+        {isLoading ? (
+          <div className="flex-1">
+            <Spinner />
+          </div>
+        ) : (
+          <input
+            disabled={disabled}
+            type="number"
+            placeholder="0.0"
+            value={value}
+            className="dark:text-white border-none appearance-none outline-none whitespace-nowrap overflow-ellipsis w-full text-3xl flex-1 bg-transparent"
+            onChange={onChange}
+          />
+        )}
         {token && <Typography variant="bodyXXL">{token}</Typography>}
       </div>
     </div>
